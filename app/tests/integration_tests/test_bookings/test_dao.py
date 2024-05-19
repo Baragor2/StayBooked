5cfa128a382a5d5babb87e1650cccb07e5d98790
Yauheni Mukhin <145256155+Baragor2@users.noreply.github.com>
2024-05-19 12:55:36 +0300

    test_booking_crud

--- app/tests/integration_tests/test_bookings/test_dao.py
+++ app/tests/integration_tests/test_bookings/test_dao.py
@@ -1,18 +1,34 @@
+import pytest
+
 from app.bookings.dao import BookingDAO
 from datetime import datetime
 
 
-async def test_add_and_get_booking():
+@pytest.mark.parametrize("user_id,room_id", [
+    (2, 2),
+    (2, 3),
+    (1, 4),
+    (1, 4),
+])
+async def test_booking_crud(user_id, room_id):
     new_booking = await BookingDAO().add(
-        user_id=2,
-        room_id=2,
+        user_id=user_id,
+        room_id=room_id,
         date_from=datetime.strptime("2024-01-10", "%Y-%m-%d"),
         date_to=datetime.strptime("2024-01-23", "%Y-%m-%d"),
     )
 
-    assert new_booking.user_id == 2
-    assert new_booking.room_id == 2
+    assert new_booking.user_id == user_id
+    assert new_booking.room_id == room_id
 
-    new_booking = await BookingDAO.find_by_id(new_booking.id)
+    new_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
 
     assert new_booking is not None
+
+    await BookingDAO.delete(
+        model_id=new_booking.id,
+        user_id=user_id
+    )
+
+    deleted_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
+    assert deleted_booking is None
