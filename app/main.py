27acbc88d422a1b98608678ff812b7f792cda70b
Yauheni Mukhin <145256155+Baragor2@users.noreply.github.com>
2024-05-20 11:55:26 +0300

    logger + sentry

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+import time
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from fastapi import Request
 from redis import asyncio as aioredis
 from sqladmin import Admin
+import sentry_sdk
 
+from app.logger import logger
 from app.admin.auth import authentication_backend
 from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UserAdmin
 from app.bookings.router import router as router_bookings
@@ -18,6 +23,14 @@ from app.pages.router import router as router_pages
 from app.users.router import router as router_users
 
 app = FastAPI()
+
+
+sentry_sdk.init(
+    dsn="https://4d1ba1405871e51bae0b237132f48c8d@o4507287845273600.ingest.de.sentry.io/4507287859560528",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
+
 
 app.mount(
     "/static",
@@ -70,3 +83,14 @@ admin.add_view(UserAdmin)
 admin.add_view(BookingsAdmin)
 admin.add_view(HotelsAdmin)
 admin.add_view(RoomsAdmin)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logger.info("Request handling time", extra={
+        "process_time": round(process_time, 4)
+    })
+    return response
